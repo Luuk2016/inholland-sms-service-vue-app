849d8704dc46a5a113d757f6bfe3bfc6542b8be3
Casper van Genderingen <c.vangenderingen@hotmail.com>
2023-01-26 16:26:31 +0100

    Added send message function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,41 +5,47 @@
       <h2 class="mt-3 mt-lg-5">Send message</h2>
 
       <div class="row g-3">
-          <div class="input-group">
-            <span class="input-group-text">Location *</span>
-            <select class="form-select" v-model="selected_location" @change="getGroupByLocation()">
-              <option :value="null" disabled>Please select a location</option>
-              <option
-                v-for="location in locations"
-                :key="location.id"
-                :value="location.id"
-              >
-                {{ location.name }}
-              </option>
-            </select>
-          </div>
-          
         <div class="input-group">
-          <input class="form-check-input" type="checkbox" v-model="all_groups" value="Test">
+          <span class="input-group-text">Location *</span>
+          <select class="form-select" v-model="selected_location" @change="getGroupByLocation()">
+            <option :value="null" disabled>Please select a location</option>
+            <option v-for="location in locations" :key="location.id" :value="location.id">
+              {{ location.name }}
+            </option>
+          </select>
+        </div>
+
+        <div class="input-group">
+          <input class="form-check-input" type="checkbox" v-model="all_groups" value="test"
+            @change="getGroupByLocation()">
           <label class="form-check-label" for="flexCheckDefault">All groups</label>
         </div>
 
-          <div class="input-group">
-            <span class="input-group-text">Group *</span>
-            <select class="form-select">
-              <option :value="null" disabled>Please select a group</option>
-              <option
-                v-for="group in groups"
-                :key="group.id"
-                :value="group.id"
-              >
-                {{ group.name }}
-              </option>
-            </select>
-          </div>
+        <div class="input-group">
+          <span class="input-group-text">Group *</span>
+          <select class="form-select" v-model="selected_group" :disabled="all_groups">
+            <option :value="null" disabled>Please select a group</option>
+            <option v-for="group in groups" :key="group.id" :value="group.id">
+              {{ group.name }}
+            </option>
+          </select>
         </div>
 
-      <p>TODO: Create form here</p>
+        <div class="input-group">
+          <span class="input-group-text">Message *</span>
+          <input type="text" class="form-control" v-model="SMS.Message">
+        </div>
+
+        <div class="input-group">
+          <span class="input-group-text">Schedule message</span>
+          <Datepicker v-model="SMS.Scheduled_at"></Datepicker>
+        </div>
+
+        <div class="input-group">
+          <button type="button" class="btn btn-primary" @click="sendMessage()">Send message</button>
+        </div>
+
+      </div>
     </div>
   </section>
 </template>
@@ -49,11 +55,14 @@ import { defineComponent } from "vue";
 import Navigation from "@/components/Navigation.vue";
 import axios from "../../util/axios";
 import { useToast } from "vue-toastification";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 export default defineComponent({
   name: "HomeView",
   components: {
     Navigation,
+    Datepicker
   },
   setup() {
     const toast = useToast();
@@ -61,11 +70,17 @@ export default defineComponent({
   },
   data() {
     return {
-      message: "",
+      SMS: {
+        Message: "",
+        Scheduled_at: new Date,
+        From_phone_number: "012345678"
+      },
       locations: [],
       groups: [],
       all_groups: false,
       selected_location: "",
+      selected_group: "",
+      url: "",
     };
   },
   async mounted() {
@@ -73,7 +88,6 @@ export default defineComponent({
   },
   methods: {
     async getLocations() {
-      console.log(this.locations)
       await axios(true)
         .get("/locations")
         .then((result) => {
@@ -88,25 +102,62 @@ export default defineComponent({
             error.response?.data || "Locations couldn't be retrieved"
           );
         });
-  },
-  async getGroupByLocation() {
-    console.log(this.selected_location);
-    
+    },
+    async getGroupByLocation() {
+      this.groups = []
       await axios(true)
-        .get("location/ + " + this.selected_location + "/groups")
+        .get("locations/" + this.selected_location + "/groups")
         .then((result) => {
           if (result.data.length !== 0) {
             this.groups = result.data;
           } else {
-            this.toast.info("No locations could be found");
+            this.toast.info("No groups could be found for this location");
           }
         })
         .catch((error) => {
           this.toast.error(
-            error.response?.data || "Locations couldn't be retrieved"
+            error.response?.data || "Groups couldn't be retrieved"
           );
         });
+
+      if (this.all_groups) {
+        this.groups = []
+        return
+      }
+    },
+    async sendMessage() {
+      if(this.selected_location == "" || (!this.all_groups && this.selected_group == "") || this.SMS.Message == ""){
+        this.toast.error("Please fill in all fields");
+        return
+      }
+
+      if (this.all_groups) {
+        this.url = "http://127.0.0.1:3000/send/locations/" + this.selected_location
+      } else {
+        this.url = "http://127.0.0.1:3000/send/groups/" + this.selected_group
+      }
+
+      if(this.SMS.Scheduled_at == null){
+        // Set date to today if it's empty
+        this.SMS.Scheduled_at = new Date
+      }
+
+      await axios(false)
+        .post(this.url, this.SMS)
+        .then(() => {
+          this.toast.success("SMS send.");
+          this.initialState()
+        })
+        .catch((error) => {
+          this.toast.error(error.response?.data || "Something went wrong");
+        });
+    },
+    initialState(){
+      this.selected_group = ""
+      this.selected_location = ""
+      this.SMS.Message = ""
+      this.SMS.Scheduled_at = new Date
+    }
   }
-}
 });
 </script>
